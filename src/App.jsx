64f3c808985cb837e838775285c4116c61b53c7b
Driih Mendes <151851739+adrielymendes@users.noreply.github.com>
2024-08-21 11:00:14 -0300

    projeto_limpo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+ export function App(){
+
+  return(
+    <>
+      <h1>Título</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore quis ipsam, nam quia ratione autem earum vero aliquam aliquid veritatis hic consequuntur aspernatur corrupti maxime, temporibus laboriosam blanditiis amet error!</p>
+    </>
+  
+  )
+}
+
