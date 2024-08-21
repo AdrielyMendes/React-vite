8bfd1dd368d08612ecd3735be0464df39bfcc7f3
Driih Mendes <151851739+adrielymendes@users.noreply.github.com>
2024-08-21 11:58:36 -0300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
+import { Header } from "./components/Header"
+import { Main } from "./components/Main"
+import { Footer } from "./components/Footer"
 
  export function App(){
 
   return(
     <>
-      <h1>Título</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore quis ipsam, nam quia ratione autem earum vero aliquam aliquid veritatis hic consequuntur aspernatur corrupti maxime, temporibus laboriosam blanditiis amet error!</p>
+       <Header/>
+        <Main/>
+        <Footer/>
+      
     </>
   
   )
