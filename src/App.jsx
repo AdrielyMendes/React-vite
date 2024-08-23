341bd9f494cab056857a20d44ca7243c0ca71692
Hellenn3 <adrielymen@gmail.com>
2024-08-23 11:50:07 -0300

    Componentes.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
-import { Header } from "./components/Header"
-import { Main } from "./components/Main"
-import { Footer } from "./components/Footer"
+import { Header } from "./components/Header/Header"
+import { Main } from "./components/Main/Main"
+import { Footer } from "./components/Footer/Footer"
 
  export function App(){
 
   return(
     <>
        <Header/>
-        <Main/>
-        <Footer/>
+        <Main>
+          <h3>Conteúdo</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, reiciendis optio. Veniam dolorum expedita blanditiis odit impedit aspernatur ratione doloremque animi minima! Autem error pariatur et. At quisquam voluptas nihil.</p>
+        </Main>
+      
+        <Footer autor="Rafael"/>
+
       
     </>
   
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import styles from './Main.module.css'
+
+export function Main(props) {
+
+    const {children} = props
+
+    return (
+        <div className={styles.Main}>
+            {children}
+        </div>
+    );
+}
+
+
